821ddaf8961f2833e011f7158845f21a4e285338
musamoh <musamoh2007@gmail.com>
2025-04-02 20:31:42 +1100

    url

--- backend/urls.py
+++ backend/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path("api/v1/", include("djoser.urls")),
     path("api/v1/", include("djoser.urls.jwt")),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/v1/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/v1/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
